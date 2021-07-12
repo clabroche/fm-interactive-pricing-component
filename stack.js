2a8e5698567314e9ab45f7739df345a9a299593b
clabroche <corentin.labroche@sailendra.fr>
2021-07-12 12:08:39 +0200

    fix(stack-monitor): add stack monitor

---
+++ stack.js
@@ -0,0 +1,21 @@
+const stack = [
+  {
+    label: 'interactive-pricing-component-front',
+    description: 'Your challenge is to build out this interactive pricing component',
+    git: {
+      home: 'https://github.com/clabroche/fm-interactive-pricing-component',
+      remote: 'git@github.com:clabroche/fm-interactive-pricing-component.git'
+    },
+    url: 'http://localhost:8080',
+    spawnCmd: 'npm',
+    spawnArgs: ['run', 'client'],
+    spawnOptions: {
+      cwd: __dirname,
+      env: {
+
+      }
+    }
+  },
+]
+
+module.exports = stack
